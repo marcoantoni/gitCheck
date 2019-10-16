55b6622e6c5acee34b8630fae8941d36b6b4c66f
Marco Antoni <marco.antoni910@gmail.com>
2019-10-15 21:47:09 -0300

    Busca ao inserir o link do repositório; mudança na exibição das datas

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,155 @@
 var urlApi 		= 'https://api.github.com/';
-var urlProject 	= 'https://github.com/marcoantoni/fundeb';
+var urlProject 	= 'https://github.com/jviriato/acg-front/';
 var user = '';
 var repo = '';
 
-urlSplit(urlProject);
+$( "#buscar" ).click(function(evt) {
+ 	alert( "Handler for .blur() called." );
+  	evt.preventDefault();
+  	urlProject = $("#url").val();
 
-$( "#buscar" ).click(function() {
-  alert( "Handler for .blur() called." );
-  urlProject = $("#url").val();
-  console.log(urlProject);
+	urlSplit(urlProject);
+
+	// estatiscas do repositorio
+	// pega a quantidade total de commits por usuario e linhas add/rem por semana
+	$.ajax({
+		type: "GET",
+		url: urlApi + "repos/" + user + "/" + repo + "/stats/contributors",
+		contentType: "application/json",
+		dataType: "json"
+	})
+	.done(function(data) {
+		var totalCommits = 0;
+		var linhasAddSemana = [];
+		var linhasDelSemana = [];
+		var commitsSemana = [];
+		var semanas = [];
+		$.each( data, function( key, value ) {
+			totalCommits += data[key].total;
+			$('#contribuidores').append("<p>" + data[key].author['login'] + " (" + data[key].total + ")</p>" );
+
+			for (let i=0; i < data[key].weeks.length; i++){
+				if (key == 0){
+					linhasAddSemana[i] = data[key].weeks[i].a;
+					linhasDelSemana[i] = data[key].weeks[i].d;
+					commitsSemana[i] = data[key].weeks[i].c;
+					semanas[i] = convertTimeStamp(data[key].weeks[i].w)
+				} else {
+					linhasAddSemana[i] = parseInt(linhasAddSemana[i]) + parseInt(data[key].weeks[i].a);
+					linhasDelSemana[i] = parseInt(linhasDelSemana[i]) + parseInt(data[key].weeks[i].d);
+					commitsSemana[i] = parseInt(commitsSemana[i]) + parseInt(data[key].weeks[i].c);
+				}
+			}
+		});
+		
+		// gráfico da quantidade de linhas adicionadas/removidas e commits por semana
+		var myChart = Highcharts.chart('graphics', {
+	        chart: {
+	            type: 'line'
+	        },
+	        title: {
+	            text: 'Dados do GitHub'
+	        },
+	        yAxis: {
+	            title: {
+	                text: 'Estatistícas'
+	            }
+	        },
+	        series: [{
+	            name: 'Linhas adicionadas',
+	        }, {
+	            name: 'Linhas removidas',
+	        }, {
+	        	name: 'Commits'
+	        }]
+	    });
+
+	var myChart2 = Highcharts.chart('graphics2', {
+	        chart: {
+	            type: 'line'
+	        },
+	        title: {
+	            text: 'Dados do GitHub2'
+	        },
+	        yAxis: {
+	            title: {
+	                text: 'Estatistícas'
+	            }
+	        },
+	        series: [{
+	            name: 'Linhas adicionadas',
+	        }, {
+	            name: 'Linhas removidas',
+	        }, {
+	        	name: 'Commits'
+	        }]
+	    });
+
+		var semanas2 = []; 
+		for (let i=0; i < semanas.length; i++){
+			if (linhasAddSemana[i] > 0 || linhasDelSemana[i] > 0 || commitsSemana[i] > 0){
+				myChart.series[0].addPoint(linhasAddSemana[i]);
+				myChart.series[1].addPoint(linhasDelSemana[i]);
+				myChart.series[2].addPoint(commitsSemana[i]);
+				semanas2.push(semanas[i]);
+			}
+		}
+
+		myChart.xAxis[0].update({
+	    	categories: semanas2
+		});
+
+		$('#totalCommits').append(totalCommits);
+	})
+	.fail(function() {
+		alert( "error" );
+	})
+
+	// listando resumo dos commits
+	$.ajax({
+		type: 'GET',
+		url: urlApi + 'repos/' + user + '/' + repo + '/commits',
+		contentType: 'application/json',
+		dataType: 'json'
+	})
+	.done(function(data) {	
+		$('#lastcommit').append(convertDate(data[0].commit.author.date) );
+		$.each( data, function(key, value) {
+			var markup = "<tr>";
+			markup +="<td>"+data[key].commit.author.name+"</td>";
+			markup +="<td>"+data[key].commit.message+"</td>";
+			markup +="<td>"+ convertDate(data[key].commit.author.date)+"</td>";
+			markup +="<td><a href='" + data[key].html_url + "'><span class='ls-ico-link'></span></a></td></tr>";
+			
+	        $('#resumoCommits tbody').append(markup);
+		});
+	})
+
+	// busca todos os issues do repositório
+	$.ajax({
+		type: "GET",
+		url: urlApi + "repos/" + user + "/" + repo + "/issues?state=all",
+		contentType: "application/json",
+		dataType: "json"
+	})
+	.done(function(data) {
+		console.log("list issues");
+		$.each( data, function(key, value) {
+			var markup = '<tr>';
+			markup +='<td><a href="#" onclick="getIssue('+value.number+');">#'+value.number+' '+ value.title +'</a>';
+			if (value.state == 'open')
+				markup += ' <span class="ls-tag-primary">Aberto</span';
+			else if (value.state == 'closed')
+				markup += ' <span class="ls-tag-info">Fechado</span';
+			markup +='</td>';
+			markup +='<td>'+value.user.login+'</td>';
+			
+			markup +='</td>';
+
+			$('#issues').append(markup);
+		});
+	})
+	
 });
 
 function urlSplit(url){
@@ -17,9 +158,18 @@ function urlSplit(url){
 	repo = segments[4];
 }
 
+/**
+ * Summary. (use period)
+ *
+ * Description 							Converte um timestamp para o formato de data/horário humano
+ * @param int unixtimestamp           	Timestamp a ser convertido.
+ * @param String formato 	          	Formato de retorno. 'hora' => retorna somente a hora. 'dia' => retorna somente o dia, 'dh' => retorna a hora e o dia
+ * @link 								https://makitweb.com/convert-unix-timestamp-to-date-time-with-javascript/
+ * @return {type} Return value description.
+ */
 function convertTimeStamp(unixtimestamp){
 	// Months array
-	var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	var months_arr = ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'];
 	// Convert timestamp to milliseconds
 	var date = new Date(unixtimestamp*1000);
 	// Year
@@ -28,155 +178,35 @@ function convertTimeStamp(unixtimestamp){
 	var month = months_arr[date.getMonth()];
 	// Day
 	var day = date.getDate();
-	// Display date time in MM-dd-yyyy h:m:s format
-	var convdataTime = day + '/' + month+ '/' +year;
-	return convdataTime;
+	// Hours
+	var hours = date.getHours();
+	// Minutes
+	var minutes = "0" + date.getMinutes();
+
+	return day + '/' + month+ '/' + year;// + ' às ' + hours + ':' + minutes;
+	 
+	//console.log(day + '/' + month+ '/' + year + ' às ' + hours + ':' + minutes);
+	//return day + '/' + month+ '/' +year;
 }
 
-$.ajax({
-	type: "GET",
-	url: urlApi + "repos/" + user + "/" + repo + "/stats/contributors",
-	contentType: "application/json",
-	dataType: "json",
-	data: JSON.stringify({
-		"content": "aGVsbG8=",
-		"encoding": "utf-8",
-		"x-requested-with": "xhr" 
-	})
-})
-.done(function(data) {
-	var totalCommits = 0;
-	var linhasAddSemana = [];
-	var linhasDelSemana = [];
-	var commitsSemana = [];
-	var semanas = [];
-	$.each( data, function( key, value ) {
-		totalCommits += data[key].total;
-		$('#contribuidores').append("<p>" + data[key].author['login'] + " (" + data[key].total + ")</p>" );
-
-		for (let i=0; i < data[key].weeks.length; i++){
-			if (key == 0){
-				linhasAddSemana[i] = data[key].weeks[i].a;
-				linhasDelSemana[i] = data[key].weeks[i].d;
-				commitsSemana[i] = data[key].weeks[i].c;
-				semanas[i] = convertTimeStamp(data[key].weeks[i].w)
-			} else {
-				linhasAddSemana[i] = parseInt(linhasAddSemana[i]) + parseInt(data[key].weeks[i].a);
-				linhasDelSemana[i] = parseInt(linhasDelSemana[i]) + parseInt(data[key].weeks[i].d);
-				commitsSemana[i] = parseInt(commitsSemana[i]) + parseInt(data[key].weeks[i].c);
-			}
-		}
-	});
-
-	var myChart = Highcharts.chart('graphics', {
-        chart: {
-            type: 'line'
-        },
-        title: {
-            text: 'Dados do GitHub'
-        },
-        yAxis: {
-            title: {
-                text: 'Estatistícas'
-            }
-        },
-        series: [{
-            name: 'Linhas adicionadas',
-        }, {
-            name: 'Linhas removidas',
-        }, {
-        	name: 'Commits'
-        }]
-    });
-
-	var semanas2 = []; 
-	for (let i=0; i < semanas.length; i++){
-		if (linhasAddSemana[i] > 0 || linhasDelSemana[i] > 0 || commitsSemana[i] > 0){
-			myChart.series[0].addPoint(linhasAddSemana[i]);
-			myChart.series[1].addPoint(linhasDelSemana[i]);
-			myChart.series[2].addPoint(commitsSemana[i]);
-			semanas2.push(semanas[i]);
-		}
-	}
-
-	myChart.xAxis[0].update({
-    	categories: semanas2
-	});
-
-	$('#totalCommits').append(totalCommits);
-})
-.fail(function() {
-	alert( "error" );
-})
-
-$.ajax({
-	type: "GET",
-	url: urlApi + "repos/" + user + "/" + repo + "/commits",
-	contentType: "application/json",
-	dataType: "json",
-	data: JSON.stringify({
-		"content": "aGVsbG8=",
-		"encoding": "utf-8",
-		"x-requested-with": "xhr" 
-	})
-})
-.done(function(data) {
-	var date = data[0].commit.author.date;
-	console.log(date);
-	$('#lastcommit').append(date);
-
-	$.each( data, function(key, value) {
-		var markup = "<tr>";
-		markup +="<td>"+data[key].commit.author.name+"</td>";
-		markup +="<td>"+data[key].commit.message+"</td>";
-		markup +="<td>"+data[key].commit.author.date+"</td>";
-		markup +="<td><a href='" + data[key].html_url + "'><span class='ls-ico-link'></span></a></td>";
-		
-        $('#resumoCommits tbody').append(markup);
-	});
-})
-
-// busca todos os issues do repositório
-console.log(urlApi + "repos/" + user + "/" + repo + "/issues?state=all");
-$.ajax({
-	type: "GET",
-	url: urlApi + "repos/" + user + "/" + repo + "/issues?state=all",
-	contentType: "application/json",
-	dataType: "json"
-})
-.done(function(data) {
-	console.log("list issues");
-	$.each( data, function(key, value) {
-		var markup = '<tr>';
-		markup +='<td><a href="#" onclick="getIssue('+value.number+');">#'+value.number+' '+ value.title +'</a>';
-		if (value.state == 'open')
-			markup += ' <span class="ls-tag-primary">Aberto</span';
-		else if (value.state == 'closed')
-			markup += ' <span class="ls-tag-info">Fechado</span';
-		markup +='</td>';
-		markup +='<td>'+value.user.login+'</td>';
-		
-		markup +='</td>';
-
-		$('#issues').append(markup);
-		console.log(value.body);
-	});
-})
+function convertDate(data){
+	var d = new Date(data);
+	return d.getDate()+'/'+d.getMonth()+'/'+d.getFullYear()+' às '+ d.getHours() + ':' +  d.getMinutes();
+}
 
 /**
  * Description Busca os dados de um issue e preenche na tabela
  * @param int issue           Id issue do GitHub.
  */
 function getIssue(issue){
-	console.log(urlApi + "repos/" + user + "/" + repo + "/issues/"+issue);
+	
 	$.ajax({
-		type: "GET",
-		url: urlApi + "repos/" + user + "/" + repo + "/issues/"+issue,
-		contentType: "application/json",
-		dataType: "json"
+		type: 'GET',
+		url: urlApi + 'repos/' + user + '/' + repo + '/issues/'+issue,
+		contentType: 'application/json',
+		dataType: 'json'
 	})
 	.done(function(data) {
-		console.log("list issues");
 		// limpando o conteudo do modal
 		$('#modalcontent').empty();
 		$('#tituloissue').empty();
@@ -201,8 +231,7 @@ function getIssue(issue){
 				markup += '  </div>';
 				markup += '</div>';
 			$('#modalcontent').append(markup);
-		locastyle.modal.open("#modalLarge");
-		console.log(data);
+		locastyle.modal.open('#modalLarge');
 	})
 
 	// get comentarios issue
@@ -210,20 +239,11 @@ function getIssue(issue){
 		type: 'GET',
 		url: urlApi + 'repos/' + user + '/' + repo + '/issues/'+issue+'/comments',
 		contentType: 'application/json',
-		dataType: 'json',
-		data: JSON.stringify({
-			'content': 'aGVsbG8=',
-			'encoding': 'utf-8',
-			'x-requested-with': "xhr" 
-		})
+		dataType: 'json'
 	})
 	.done(function(data) {
 		console.log("list issues");
 		$.each(data, function(key, value){
-			//var markup = '<p>' + value.user.login +'</p>';
-			//markup += '<p>' + value.body + '</p><hr>';
-
-			//$('#modalcontent').append( markup);
 			var markup = '<div class="ls-list">';
 				markup += '  <div class="ls-list-content ">';
 				markup += '    <div class="col-xs-12 col-md-6">';
